1c5024fe6b4677f734a5d6276ece9b29514a6b5d
Antwanson <zmyantro@gmail.com>
2025-04-21 20:13:23 -0500

    Fixed borders and enemy spawning

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,11 +6,16 @@ public class EnemySpawner : MonoBehaviour
     public float spawnInterval = 2f; // Time interval between spawns
 
     public float spawnRadius = 5f; // Radius around the spawner to spawn enemies
+
+    public GameObject[] enemySpawners;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 0f, spawnInterval);
+
+        //get all enemyspawnerpucks in scene, tagged with "EnemySpawnLocation"
+        enemySpawners = GameObject.FindGameObjectsWithTag("EnemySpawnLocation");
     }
 
     // Update is called once per frame
@@ -21,9 +26,14 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnEnemy()
     {
-        // Instantiate the enemy prefab at the spawner's position and rotation
-        Vector3 spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
-        spawnPosition.y = transform.position.y; // Keep the y position the same as the spawner
+
+        //get the location of one of the pucks in the scene using enemySpawners array (get a random one)
+        //use math random
+        int randomIndex = Random.Range(0, enemySpawners.Length);
+        GameObject randomEnemySpawner = enemySpawners[randomIndex];
+
+    
+        Vector2 spawnPosition = randomEnemySpawner.transform.position;
         GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
         // Set the enemy's parent to the spawner for organization (optional)
         enemy.transform.parent = transform;
